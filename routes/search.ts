2a87cdfafdf49924f4a89f08b2fc477f21fbcfba
FotiosPaparisvas <153185549+FotiosPaparisvas@users.noreply.github.com>
2024-12-18 13:43:53 +0200

    Fixed

--- routes/search.ts
+++ routes/search.ts
@@ -9,25 +9,18 @@ import { UserModel } from '../models/user'
 import { challenges } from '../data/datacache'
 
 import * as utils from '../lib/utils'
-import * as challengeUtils from '../lib/challengeUtils'
+const challengeUtils = require('../lib/challengeUtils')
 
 class ErrorWithParent extends Error {
   parent: Error | undefined
 }
 
 // vuln-code-snippet start unionSqlInjectionChallenge dbSchemaChallenge
-export default function searchProducts () {
+module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(
-      'SELECT * FROM Products WHERE ((name LIKE :criteria OR description LIKE :criteria) AND deletedAt IS NULL) ORDER BY name',
-      {
-        replacements: {
-          criteria: `%${criteria}%`
-        }
-      }
-    )
+    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
