65860bd9fd2f9573874708432e868adf909d152a
tmpillas <74024931+tmpillas@users.noreply.github.com>
2024-12-18 12:55:23 +0200

    Add files via upload

--- frontend/src/hacking-instructor/index.ts
+++ frontend/src/hacking-instructor/index.ts
@@ -106,16 +106,9 @@ function loadHint (hint: ChallengeHint): HTMLElement {
   picture.style.marginRight = '8px'
   picture.src = '/assets/public/images/hackingInstructor.png'
 
-    // Function to sanitize HTML content
-  function sanitizeHtml(content: string): string {
-    const element = document.createElement('div')
-    element.innerText = content
-    return element.innerHTML
-  }
-
   const textBox = document.createElement('span')
   textBox.style.flexGrow = '2'
-  textBox.innerHTML = sanitizeHtml(snarkdown(hint.text))
+  textBox.innerHTML = snarkdown(hint.text)
 
   const cancelButton = document.createElement('button')
   cancelButton.id = 'cancelButton'
