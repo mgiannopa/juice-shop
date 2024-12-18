b4d1c49f99754aba531ee05ac9c113026059c7c5
tmpillas <74024931+tmpillas@users.noreply.github.com>
2024-12-18 12:51:54 +0200

    Add files via upload

--- rsn/rsnUtil.ts
+++ rsn/rsnUtil.ts
@@ -1,7 +1,7 @@
 import { retrieveCodeSnippet } from '../routes/vulnCodeSnippet'
 import colors from 'colors/safe'
-import Diff = require('diff')
-import fs = require('fs')
+const Diff = require('diff')
+const fs = require('fs')
 const fixesPath = 'data/static/codefixes'
 const cacheFile = 'rsn/cache.json'
 
