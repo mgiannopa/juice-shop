d5d3539913e0c29afb29ad6662c01147e1e8c384
Matthaios Giannopapas <153185447+mgiannopa@users.noreply.github.com>
2024-12-18 11:10:11 +0200

    Update hacking-challenge-progress-score-card.component.ts

--- frontend/src/app/score-board/components/hacking-challenge-progress-score-card/hacking-challenge-progress-score-card.component.ts
+++ frontend/src/app/score-board/components/hacking-challenge-progress-score-card/hacking-challenge-progress-score-card.component.ts
@@ -16,35 +16,9 @@ export class HackingChallengeProgressScoreCardComponent implements OnInit, OnCha
     this.updatedNumberOfSolvedChallenges()
   }
 
-import { Component, Input, type OnChanges, type OnInit, type SimpleChanges } from '@angular/core'
-import { type EnrichedChallenge } from '../../types/EnrichedChallenge'
-
-@Component({
-  selector: 'hacking-challenge-progress-score-card',
-  templateUrl: './hacking-challenge-progress-score-card.component.html',
-  styleUrls: ['./hacking-challenge-progress-score-card.component.scss']
-})
-export class HackingChallengeProgressScoreCardComponent implements OnInit, OnChanges {
-  @Input()
-  public allChallenges: EnrichedChallenge[] = []
-
-  public solvedChallenges: number
-
-  ngOnInit (): void {
+  ngOnChanges (changes: SimpleChanges): void {
     this.updatedNumberOfSolvedChallenges()
   }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['allChallenges']?.currentValue) {
-      this.updatedNumberOfSolvedChallenges();
-    }
-  }
-  
-  private updatedNumberOfSolvedChallenges (): void {
-    this.solvedChallenges = this.allChallenges.filter((challenge) => challenge.solved).length
-  }
-}
-
 
   private updatedNumberOfSolvedChallenges (): void {
     this.solvedChallenges = this.allChallenges.filter((challenge) => challenge.solved).length
